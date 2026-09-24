a77923a4267dd66ea7a1981c4c8d3161c7fb7347
winceria <winceria044@gmail.com>
2026-09-24 15:50:52 +0700

    Upload project LatihanKlienServer

---
+++ src/com/pbi/tk/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbi.tk;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Program LatihanKlienServer");
+        System.out.println("Nama  : Winceria Girsang");
+        System.out.println("NIM   : 25110085");
+        System.out.println("Kelas : TK B");
+    }
+
+    }
+    
+
